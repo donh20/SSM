2b0a26975efb8302a7a2f123cc46d8b5227bc076
Dong Hao <haodong.donh@gmail.com>
2022-10-24 00:28:46 +0800

    spring-aop

---
+++ spring_aop/src/test/java/com/atguigu/spring/test/AOPByAnnotationTest.java
@@ -0,0 +1,17 @@
+package com.atguigu.spring.test;
+
+import com.atguigu.spring.aop.annotation.Calculator;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class AOPByAnnotationTest {
+    @Test
+    public void AOPByAnnotation(){
+        ApplicationContext ioc = new ClassPathXmlApplicationContext("aop-annotation.xml");
+        Calculator calculator = ioc.getBean(Calculator.class);
+        calculator.add(3,1);
+        /*calculator.div(1,1);*/
+
+    }
+}
