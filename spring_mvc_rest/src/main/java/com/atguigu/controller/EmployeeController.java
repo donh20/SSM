cfc63bcfa6e2b42ce533a9fefe335e65a233cf8d
Dong Hao <haodong.donh@gmail.com>
2022-11-06 15:07:10 +0800

    springMVC restful

---
+++ spring_mvc_rest/src/main/java/com/atguigu/controller/EmployeeController.java
@@ -0,0 +1,35 @@
+package com.atguigu.controller;
+
+import com.atguigu.dao.EmployeeDao;
+import com.atguigu.pojo.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Collection;
+
+/**
+ * 查询所有的员工信息-->/employee-->get
+ * 跳转到添加页面-->/to/add-->get
+ * 新增员工信息-->/employee-->post
+ * 跳转到修改页面-->/employee/1-->get
+ * 修改员工信息-->/employee-->put
+ * 删除员工信息-->/employee/1-->delete
+ */
+@Controller
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeDao employeeDao;
+
+    @RequestMapping(value = "/employee",method = RequestMethod.GET)
+    public String getAllEmployee(Model model){
+        Collection<Employee> allEmployees = employeeDao.getAll();
+        model.addAttribute("allEmployee", allEmployees);
+        return "employee_list";
+    }
+
+}
