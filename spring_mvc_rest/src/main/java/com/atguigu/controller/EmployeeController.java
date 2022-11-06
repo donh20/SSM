82128b56f2402a5bb2ffa7f7c7b6fde02dd3aa80
Dong Hao <haodong.donh@gmail.com>
2022-11-06 20:51:56 +0800

    springMVC restful employee add delete get put

--- spring_mvc_rest/src/main/java/com/atguigu/controller/EmployeeController.java
+++ spring_mvc_rest/src/main/java/com/atguigu/controller/EmployeeController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.nio.file.Path;
 import java.util.Collection;
 
 /**
@@ -27,8 +28,11 @@ public class EmployeeController {
 
     @RequestMapping(value = "/employee",method = RequestMethod.GET)
     public String getAllEmployee(Model model){
+        //获取所有的员工信息
         Collection<Employee> allEmployees = employeeDao.getAll();
+        //将所有的员工信息在请求域中共享
         model.addAttribute("allEmployee", allEmployees);
+        //跳转到列表页面
         return "employee_list";
     }
 
@@ -41,4 +45,29 @@ public class EmployeeController {
         return "redirect:/employee";
     }
 
+    @RequestMapping(value = "/employee/{id}",method = RequestMethod.GET)
+    public String toUpdate(@PathVariable("id") Integer id, Model model){
+        //根据id查询员工信息
+        Employee employee = employeeDao.get(id);
+        //将员工信息共享到请求域中
+        model.addAttribute("employee",employee);
+        //跳转到employee_update
+        return "employee_update";
+    }
+
+    @RequestMapping(value = "/employee",method = RequestMethod.PUT)
+    public String updateEmployee(Employee employee){
+        //修改员工信息
+        employeeDao.save(employee);
+        //重定向到列表功能：/employee
+        return "redirect:/employee";
+    }
+
+    @RequestMapping(value = "/employee/{id}",method = RequestMethod.DELETE)
+    public String deteleMethod(@PathVariable("id") Integer id){
+        //删除员工信息
+        employeeDao.delete(id);
+        //重定向到列表功能: /employee
+        return "redirect:/employee";
+    }
 }
