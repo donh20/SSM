3a03c686c3ca3f0546c84f6c3376b63242f795c2
Dong Hao <haodong.donh@gmail.com>
2022-11-07 00:19:10 +0800

    spring_mvc_ajax axios responseBody json

--- spring_mvc_ajax/src/main/java/com/atguigu/controller/TestAjaxController.java
+++ spring_mvc_ajax/src/main/java/com/atguigu/controller/TestAjaxController.java
@@ -5,9 +5,12 @@ import com.sun.deploy.net.HttpResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -16,6 +19,12 @@ import java.util.Map;
  * a>导入jackson的依赖
  * b>在SpringMVC的配置文件中设置<mvc:annotation-driven />
  * c>在处理请求的控制器方法的形参位置,直接设置json格式的请求参数要转换的java类型的形参,使用@RequestBody注解标识即可
+ * 3、@ResponseBody：将所标识的控制器方法的返回值作为响应报文的响应体响应到浏览器
+ * 4、使用@ResponseBody注解响应浏览器json格式的数据
+ * a>导入jackson的依赖
+ * b>在SpringMVC的配置文件中设置<mvc:annotation-driven />
+ * c>将需要转换为json字符串的java对象直接作为控制器方法的返回值，使用@ResponseBody注解标识控制器方法
+ * 就可以将java对象直接转换为json字符串，并响应到浏览器
  */
 @Controller
 public class TestAjaxController {
@@ -40,5 +49,21 @@ public class TestAjaxController {
     public void testRequestBody(@RequestBody User user, HttpServletResponse response) throws IOException {
         System.out.println(user);
         response.getWriter().write("hello, ResponseBody.");
+    }
+
+    @RequestMapping("/test/ResponseBody")
+    @ResponseBody
+    public String testResponseBody(){
+        return "success";
+    }
+
+    @RequestMapping("/test/ResponseBody/json")
+    @ResponseBody
+    public List<User> testResponseBodyJson(){
+        User user1 = new User(1001, "admin1", "123456", 20, "男");
+        User user2 = new User(1002, "admin2", "123456", 20, "男");
+        User user3 = new User(1003, "admin3", "123456", 20, "男");
+        List<User> list = Arrays.asList(user1, user2, user3);
+        return list;
     }
 }
