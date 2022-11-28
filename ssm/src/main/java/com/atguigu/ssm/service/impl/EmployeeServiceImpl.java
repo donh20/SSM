3630cd2b7430c4ce504d22b1cd250de28e62c3df
Dong Hao <haodong.donh@gmail.com>
2022-11-28 20:53:35 +0800

    ssm springmvc spring

--- ssm/src/main/java/com/atguigu/ssm/service/impl/EmployeeServiceImpl.java
+++ ssm/src/main/java/com/atguigu/ssm/service/impl/EmployeeServiceImpl.java
@@ -1,8 +1,14 @@
 package com.atguigu.ssm.service.impl;
 
 import com.atguigu.ssm.service.EmployeeService;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
+
+    @Autowired
+    private SqlSessionFactory sqlSessionFactory;
+    
 }
