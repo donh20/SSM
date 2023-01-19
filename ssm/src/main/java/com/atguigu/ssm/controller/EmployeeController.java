cb83d179f7897b5f1036f62b8ca8421da83b0e12
haodong <haodong.donh@gmail.com>
2023-01-19 17:08:17 +0800

    ssm-查询员工分页信息

--- ssm/src/main/java/com/atguigu/ssm/controller/EmployeeController.java
+++ ssm/src/main/java/com/atguigu/ssm/controller/EmployeeController.java
@@ -2,9 +2,11 @@ package com.atguigu.ssm.controller;
 
 import com.atguigu.ssm.pojo.Employee;
 import com.atguigu.ssm.service.EmployeeService;
+import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -12,6 +14,7 @@ import java.util.List;
 
 /**
  * 查询所有的员工信息-->/employee-->get
+ * 查询员工分页信息-->/employee/page/1-->get
  * 根据id查询员工信息-->/employee/1-->get
  * 跳转到添加页面-->/to/add-->get
  * 添加员工信息-->/employee-->post
@@ -24,6 +27,16 @@ public class EmployeeController {
 
     @Autowired
     private EmployeeService employeeService;
+
+    @RequestMapping(value = "/employee/page/{pageNum}", method = RequestMethod.GET)
+    public String getEmployeePage(@PathVariable("pageNum") Integer pageNum, Model model){
+        //获取员工的分页信息
+        PageInfo<Employee> page = employeeService.getEmployeePage(pageNum);
+        //将分页数据共享到请求域中
+        model.addAttribute("page", page);
+        //跳转到employee_list
+        return "employee_list";
+    }
 
     @RequestMapping(value = "/employee",method = RequestMethod.GET)
     public String getAllEmployee(Model model){
--- ssm/src/main/java/com/atguigu/ssm/service/impl/EmployeeServiceImpl.java
+++ ssm/src/main/java/com/atguigu/ssm/service/impl/EmployeeServiceImpl.java
@@ -3,6 +3,8 @@ package com.atguigu.ssm.service.impl;
 import com.atguigu.ssm.mapper.EmployeeMapper;
 import com.atguigu.ssm.pojo.Employee;
 import com.atguigu.ssm.service.EmployeeService;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -22,5 +24,16 @@ public class EmployeeServiceImpl implements EmployeeService {
     @Override
     public List<Employee> getAllEmployee() {
         return employeeMapper.getAllEmployee();
+    }
+
+    @Override
+    public PageInfo<Employee> getEmployeePage(Integer pageNum) {
+        //开启分页功能
+        PageHelper.startPage(pageNum, 4);
+        //查询所有的员工信息，本质上是一个拦截器，加limit功能来实现分页
+        List<Employee> list = employeeMapper.getAllEmployee();
+        //获取分页相关数据
+        PageInfo<Employee> page = new PageInfo<>(list, 5);
+        return page;
     }
 }
